2c322858c4d5dff2ed1a9f3623350a47edfd5b92
wgabbp <144801367+wgabbp@users.noreply.github.com>
2024-11-19 08:11:53 -0300

    19.9

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'BIOLOGIA',
+    'O que são linfócitos?',
+    'Principais células do sistema imunológico'
+)
+criaCartao(
+    'BIOLOGIA',
+    'Quais são os tipos de linfócitos?',
+    'T,B e NK'
+)
+criaCartao(
+    'BIOLOGIA',
+    'Quais são os representantes do reino Monera?',
+    'Bactérias, cianobactérias e arqueobactérias.'
+)
+criaCartao(
+    'BIOLOGIA',
+    'Qual é a função das veias?',
+    'Transportar sangue do corpo para o coração.'
+)
+criaCartao(
+    'BIOLOGIA',
+    'Qual é a função das artérias?',
+    'Transportar sangue do coração para o corpo.'
+)
+criaCartao(
+    'BIOLOGIA',
+    'Qual é a função do sangue?',
+    'Transportar nutrientes e oxigênio para o corpo.'
+)
