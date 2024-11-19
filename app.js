187bd10bbf26548db4008c856bb5dea8109f8241
wgabbp <144801367+wgabbp@users.noreply.github.com>
2024-11-19 08:23:33 -0300

    19.11.24

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container')
       let cartao = document.createElement('article')
@@ -14,12 +15,9 @@ function criaCartao(categoria, pergunta, resposta){
             </div>  
             `
 let respostaEstaVisivel = false
-
 function viraCartao(){
     respostaEstaVisivel = !respostaEstaVisivel
     cartao.classList.toggle('active', respostaEstaVisivel)
 }
 cartao.addEventListener('click', viraCartao)
-
 container.appendChild(cartao)
-}
