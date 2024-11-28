a30e8203bd25b2ccd20b89293e380c36f94977df
wgabbp <144801367+wgabbp@users.noreply.github.com>
2024-11-28 17:28:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function criaCartao(categoria, pergunta, resposta){
                 <div class="cartao_conteudo_pergunta">
                     <p>${pergunta}</p>
                 </div>
-                <div class="cartao_conteudo-resposta">
+                <div class="cartao_conteudo_resposta">
                     <p>${resposta}</p>
                 </div>
             </div>  
